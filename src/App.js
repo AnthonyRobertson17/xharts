6a6be1f7e1a07f5a03388654436249139a6a1981
Kaiser Dandangi <kdandangi@freshbooks.com>
2019-11-13 23:17:07 -0500

    import react-dates default stylesheet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {DateRangePicker} from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css';
 import './App.css';
 import BackendAdapter from './backend-adapter.js'
 import LineGraph from './line-graph.js'
@@ -33,15 +34,15 @@ class App extends React.Component {
       <div className="App">
         <h1>This is a test app</h1>
         <div>
-        <DateRangePicker
-          startDate={this.state.startDate}
-          startDateId="your_unique_start_date_id"
-          endDate={this.state.endDate}
-          endDateId="your_unique_end_date_id"
-          onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
-          focusedInput={this.state.focusedInput}
-          onFocusChange={focusedInput => this.setState({ focusedInput })}
-        />
+          <DateRangePicker
+            startDate={this.state.startDate}
+            startDateId="your_unique_start_date_id"
+            endDate={this.state.endDate}
+            endDateId="your_unique_end_date_id"
+            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
+            focusedInput={this.state.focusedInput}
+            onFocusChange={focusedInput => this.setState({ focusedInput })}
+          />
         </div>
         <div>
           <LineGraph/>
