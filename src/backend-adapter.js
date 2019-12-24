a0f27828f561785d77c9b761180a26f17ebb9d7e
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2019-12-24 16:17:19 -0500

    updated component to work with backend response

--- src/backend-adapter.js
+++ src/backend-adapter.js
@@ -2,16 +2,146 @@ const BackendAdapter = {
   getAllData: () => {
     return Promise.resolve({
       data: [
-        { datetime: "2019-11-01T19:53:06.875Z", data: { value: 4 } },
-        { datetime: "2019-11-02T19:53:06.875Z", data: { value: 3 } },
-        { datetime: "2019-11-03T19:53:06.875Z", data: { value: 9 } },
-        { datetime: "2019-11-04T19:53:06.875Z", data: { value: 4 } },
-        { datetime: "2019-11-05T19:53:06.875Z", data: { value: 4 } },
-        { datetime: "2019-11-06T19:53:06.875Z", data: { value: 2 } },
-        { datetime: "2019-11-07T19:53:06.875Z", data: { value: 1 } },
-        { datetime: "2019-11-08T19:53:06.875Z", data: { value: 0 } },
-        { datetime: "2019-11-09T19:53:06.875Z", data: { value: 7 } },
-        { datetime: "2019-11-10T19:53:06.875Z", data: { value: 4 } },
+        {
+          "id": 1,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 9,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:38:39.560041",
+          "updated_at": "2019-12-24T17:38:39.560041"
+        },
+        {
+          "id": 2,
+          "metric_name": "dat",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 9,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:39:15.176718",
+          "updated_at": "2019-12-24T17:39:15.176718"
+        },
+        {
+          "id": 3,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 10,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:39:39.376640",
+          "updated_at": "2019-12-24T17:39:39.376640"
+        },
+        {
+          "id": 4,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 3,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:40:42.498604",
+          "updated_at": "2019-12-24T17:40:42.498604"
+        },
+        {
+          "id": 5,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 4,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:41:04.491320",
+          "updated_at": "2019-12-24T17:41:04.491320"
+        },
+        {
+          "id": 6,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 3,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:41:09.888987",
+          "updated_at": "2019-12-24T17:41:09.888987"
+        },
+        {
+          "id": 7,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 8,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:41:13.688332",
+          "updated_at": "2019-12-24T17:41:13.688332"
+        },
+        {
+          "id": 8,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 4,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:41:40.985647",
+          "updated_at": "2019-12-24T17:41:40.985647"
+        },
+        {
+          "id": 9,
+          "metric_name": "dat",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 5,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:41:47.353822",
+          "updated_at": "2019-12-24T17:41:47.353822"
+        },
+        {
+          "id": 10,
+          "metric_name": "who",
+          "data": {
+            "cloud": "gcp",
+            "cluster": "alpha-1",
+            "decimal_revision": 9,
+            "latency": 9,
+            "release": "1911.1.0",
+            "revision": 9
+          },
+          "created_at": "2019-12-24T17:41:58.921534",
+          "updated_at": "2019-12-24T17:41:58.921534"
+        }
       ],
       meta: {
         total: 10,
--- src/line-graph.js
+++ src/line-graph.js
@@ -3,7 +3,7 @@ import { ResponsiveLine } from '@nivo/line'
 
 function formatMetrics(metrics) {
   const formatted = {};
-  (metrics.data || []).forEach(metric => {
+  (metrics || []).forEach(metric => {
     formatted[metric.metric_name] = formatted[metric.metric_name] ||
       {id: metric.metric_name, data: []};
 
@@ -13,157 +13,153 @@ function formatMetrics(metrics) {
     });
   });
 
-  return Object.keys(formatted).map(metric_name => formatted[metric_name]);
+  const results = Object.keys(formatted).map(metric_name => formatted[metric_name]);
+  console.log(results);
+  return results;
 }
 
-const data = {
-  data: [
-    {
-      "id": 1,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 9,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:38:39.560041",
-      "updated_at": "2019-12-24T17:38:39.560041"
+const dummyData = [
+  {
+    "id": 1,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 9,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 2,
-      "metric_name": "dat",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 9,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:39:15.176718",
-      "updated_at": "2019-12-24T17:39:15.176718"
+    "created_at": "2019-12-24T17:38:39.560041",
+    "updated_at": "2019-12-24T17:38:39.560041"
+  },
+  {
+    "id": 2,
+    "metric_name": "dat",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 9,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 3,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 10,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:39:39.376640",
-      "updated_at": "2019-12-24T17:39:39.376640"
+    "created_at": "2019-12-24T17:39:15.176718",
+    "updated_at": "2019-12-24T17:39:15.176718"
+  },
+  {
+    "id": 3,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 10,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 4,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 3,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:40:42.498604",
-      "updated_at": "2019-12-24T17:40:42.498604"
+    "created_at": "2019-12-24T17:39:39.376640",
+    "updated_at": "2019-12-24T17:39:39.376640"
+  },
+  {
+    "id": 4,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 3,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 5,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 4,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:41:04.491320",
-      "updated_at": "2019-12-24T17:41:04.491320"
+    "created_at": "2019-12-24T17:40:42.498604",
+    "updated_at": "2019-12-24T17:40:42.498604"
+  },
+  {
+    "id": 5,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 4,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 6,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 3,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:41:09.888987",
-      "updated_at": "2019-12-24T17:41:09.888987"
+    "created_at": "2019-12-24T17:41:04.491320",
+    "updated_at": "2019-12-24T17:41:04.491320"
+  },
+  {
+    "id": 6,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 3,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 7,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 8,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:41:13.688332",
-      "updated_at": "2019-12-24T17:41:13.688332"
+    "created_at": "2019-12-24T17:41:09.888987",
+    "updated_at": "2019-12-24T17:41:09.888987"
+  },
+  {
+    "id": 7,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 8,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 8,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 4,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:41:40.985647",
-      "updated_at": "2019-12-24T17:41:40.985647"
+    "created_at": "2019-12-24T17:41:13.688332",
+    "updated_at": "2019-12-24T17:41:13.688332"
+  },
+  {
+    "id": 8,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 4,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 9,
-      "metric_name": "dat",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 5,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:41:47.353822",
-      "updated_at": "2019-12-24T17:41:47.353822"
+    "created_at": "2019-12-24T17:41:40.985647",
+    "updated_at": "2019-12-24T17:41:40.985647"
+  },
+  {
+    "id": 9,
+    "metric_name": "dat",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 5,
+      "release": "1911.1.0",
+      "revision": 9
     },
-    {
-      "id": 10,
-      "metric_name": "who",
-      "data": {
-        "cloud": "gcp",
-        "cluster": "alpha-1",
-        "decimal_revision": 9,
-        "latency": 9,
-        "release": "1911.1.0",
-        "revision": 9
-      },
-      "created_at": "2019-12-24T17:41:58.921534",
-      "updated_at": "2019-12-24T17:41:58.921534"
-    }
-  ],
-  meta: {
-    total: 10,
-    page: 0
+    "created_at": "2019-12-24T17:41:47.353822",
+    "updated_at": "2019-12-24T17:41:47.353822"
+  },
+  {
+    "id": 10,
+    "metric_name": "who",
+    "data": {
+      "cloud": "gcp",
+      "cluster": "alpha-1",
+      "decimal_revision": 9,
+      "latency": 9,
+      "release": "1911.1.0",
+      "revision": 9
+    },
+    "created_at": "2019-12-24T17:41:58.921534",
+    "updated_at": "2019-12-24T17:41:58.921534"
   }
-};
+];
 
 const METRIC_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f";
 const TIME_DISPLAY_FORMAT = "%H:%M:%S";
@@ -237,9 +233,16 @@ const MyResponsiveLine = ({ graphData }) => (
   />
 );
 
-class LineGraph extends React.PureComponent {
+class LineGraph extends React.Component {
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(prevProps, prevState, snapshot);
+  }
+
   render() {
-    console.log(formatMetrics(data));
+    const { data } = this.props;
+    console.log(this.props);
+    dummyData && data && console.log(data, formatMetrics(data));
     return (
       <div style={{ height: '400px' }}>
         <MyResponsiveLine graphData={formatMetrics(data)} />
