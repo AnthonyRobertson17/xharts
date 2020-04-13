2c9169ffe8a0b3bc7ac42ce2122832600fff1048
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2020-04-12 20:31:03 -0400

    convert datetime-picker to be a stateful component
    so dates get pushed to app only on successful submission
    checks for fromDate < toDate

--- src/datetime-picker.js
+++ src/datetime-picker.js
@@ -3,49 +3,62 @@ import IconButton from '@material-ui/core/IconButton';
 import DoneIcon from '@material-ui/icons/Done';
 import Grid from '@material-ui/core/Grid';
 import { DateTimePicker, KeyboardDateTimePicker } from "@material-ui/pickers";
+import DateFnsAdapter from "@date-io/date-fns";
 
-const MyDateTimePicker = ({
-  dates = [new Date(), new Date()],
-  data,
-  metricField,
-  submitDates,
-  updateDates,
-  handleMetricFieldChange
-}) => {
-  console.log("dates", dates);
-  return (
-    <React.Fragment>
-      <Grid item>
-      <DateTimePicker
-        variant="inline"
-        label="From"
-        value={dates[0]}
-        onChange={(fromDate) => console.log("from", fromDate) || updateDates([fromDate, dates[1]])}
-        format="yyyy/MM/dd HH:mm"
-      />
-      </Grid>
+const dateFns = new DateFnsAdapter();
 
-      <Grid item>
-      <KeyboardDateTimePicker
-        variant="inline"
-        // ampm={false} // for am/pm
-        label="Till"
-        value={dates[1]}
-        onChange={(toDate) => console.log("to", toDate) || updateDates([dates[0], toDate])}
-        onError={console.log}
-        format="yyyy/MM/dd HH:mm"
-      />
-      </Grid>
+class MyDateTimePicker extends React.Component {
 
-      <Grid item>
-      <IconButton aria-label="submit" onClick={() => submitDates()}>
-        <DoneIcon />
-      </IconButton>
-      </Grid>
+  constructor(props) {
+    super(props);
 
+    const [fromDate, toDate] = this.props.dates;
+    this.state = {
+      fromDate: fromDate,
+      toDate: toDate
+    };
+  }
 
-    </React.Fragment>
-  );
+  minDateMessage(fromDate) {
+    return fromDate ? dateFns.format(fromDate, "yyyy/MM/dd HH:mm") : 'the from date';
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Grid item>
+          <KeyboardDateTimePicker
+            variant="inline"
+            label="From"
+            value={this.state.fromDate}
+            onChange={(fromDate) => console.log("from", fromDate) || this.setState({ fromDate })}
+            format="yyyy/MM/dd HH:mm"
+          />
+        </Grid>
+
+        <Grid item>
+          <KeyboardDateTimePicker
+            variant="inline"
+            // ampm={false} // for am/pm
+            label="Till"
+            value={this.state.toDate}
+            minDate={this.state.fromDate}
+            minDateMessage={`value cannot be before ${this.minDateMessage(this.state.fromDate)}`}
+            onChange={(toDate) => console.log("to", toDate) || this.setState({ toDate })}
+            onError={(e, v) => console.log("Till", "e", e, "v", v)}
+            format="yyyy/MM/dd HH:mm"
+          />
+        </Grid>
+
+        <Grid item>
+          <IconButton aria-label="submit" onClick={() => this.props.submitDates([this.state.fromDate, this.state.toDate])}>
+            <DoneIcon />
+          </IconButton>
+        </Grid>
+
+      </React.Fragment>
+    );
+  }
 }
 
 export default MyDateTimePicker;
